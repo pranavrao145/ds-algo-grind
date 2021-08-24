74de8b71bb1456612ca7a265f6d9fdf69b04e11d
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-08-24 13:35:58 -0400

    added insert to binary search tree notes (untested)

--- data-structures/binary-search-trees/binary-search-trees.cpp
+++ data-structures/binary-search-trees/binary-search-trees.cpp
@@ -15,9 +15,9 @@
 // since, by definition, a binary search tree's left and right children
 // must also be a binary search tree, we don't need to make an explicit
 // class for a node. Instead we can just use a single binary search tree class
-// In this class we'll define a search method, an insert methodm and a delete
-// method. We'll also include a utility function to print the binary search tree
-// using inorder traversal
+// In this class we'll define a constructor, a search method, an insert method
+// and a delete method. We'll also include a utility function to print the
+// binary search tree using inorder traversal.
 
 // for all methods in this data structure, the worst case time complexity is
 // O(n) where n is the height of the binary search tree. However, in the best
@@ -30,10 +30,24 @@ public:
   int data;
   BST *left, *right;
 
-  // this function will take a node and a desired key, and search the tree
+  // constructor with no predefined left and right
+  BST(int data) {
+    this->data = data;
+    this->left = nullptr;
+    this->right = nullptr;
+  }
+
+  // constructor with a predefined left and right
+  BST(int data, BST *left, BST *right) {
+    this->data = data;
+    this->left = left;
+    this->right = right;
+  }
+
+  // this function will take a root node and a desired key, and search the tree
   // recursively for a node with that key. If such a node exists, it will
   // return this node. Else, it will return a nullptr
-  BST *searchTree(BST *node, int data) {
+  BST *searchTree(BST *root, int data) {
     // this is the strategy: if the current node is null, the node is
     // not found, so return null. If the node's key is the one we're looking
     // for, we return the node. If the current node's key is greater than the
@@ -41,15 +55,44 @@ public:
     // left subtree.
 
     // check for nullptr
-    if (node == nullptr)
+    if (!root)
       return nullptr;
 
-    if (node->data == data)
-      return node;
+    if (root->data == data)
+      return root;
 
-    if (node->data > data)
-      return searchTree(node->right, data);
+    if (root->data > data)
+      return searchTree(root->right, data);
 
-    return searchTree(node->left, data);
+    return searchTree(root->left, data);
+  }
+
+  // this function will take a root node and a desired key, and insert a node
+  // with this key into the the tree in the right place. This function will
+  // return a pointer to the newly formed binary search tree.
+  BST *insert(BST *root, int key) {
+    // the node will always be a leaf, so the strategy is to check until the
+    // point where there's no child node, at which point we can insert the new
+    // node
+
+    // if the root doesn't exists (i.e. the tree doesn't exist or the previous
+    // node had no child), we can insert here because the new node must be a
+    // leaf
+    if (!root) {
+      BST *node = new BST(key); // make a new node
+    }
+    // if the given key is greater than the node's key, go to the right
+    // subtree
+    else if (root->data < key) {
+      root->right = insert(root->right, key);
+    }
+    // if the given key is less than the node's key, go to the left
+    // subtree
+    else if (root->data > key) {
+      root->left = insert(root->left, key);
+    }
+
+    return root;
   }
 };
+
